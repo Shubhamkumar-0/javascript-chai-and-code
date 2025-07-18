a548e9f253b68ac29bf6d8e8b1e499c772463450
Shubham Kumar <shubhamkumarerror1@gmail.com>
2025-07-18 07:50:58 +0000

    conversion of data types

--- 01_basics/dataTypes.js
+++ 01_basics/dataTypes.js
@@ -18,3 +18,48 @@ let isloggedin=false
  */
 console.log(typeof undefined); //undefined
 console.log(typeof null); //object
+console.log(" ");
+
+
+
+//DataType conversion.......& Operation......
+let score=33;
+console.log(typeof score);
+console.log(typeof(score));
+console.log(" ");
+
+
+let str="33abc";
+console.log(typeof str);
+console.log(typeof(str));
+console.log(" ");
+
+
+let conv=Number(str);
+console.log(typeof conv);
+console.log(conv); //NaN
+
+/**conversion in number then
+ * "33"=33
+ * "33abc"=NaN
+ * true=1;
+ * false=0;
+ */
+
+let bool=1;
+let b=Boolean(bool);//1=>true
+console.log(b);//true
+
+/*false will come only for 0 
+other wise its always true in boolean conversion
+
+1=>true; 0=>false
+""=>false (because its empty string)
+"shubham"=>true
+*/
+
+let num=33;
+let st=String(num);
+console.log(" ");
+console.log(st);//33
+console.log(typeof(st));//string
