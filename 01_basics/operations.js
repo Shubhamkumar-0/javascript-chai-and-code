91a3389f14da1d28e11f3aaddbfee6e60ae3d41c
Shubham Kumar <shubhamkumarerror1@gmail.com>
2025-07-18 08:19:31 +0000

    operation

---
+++ 01_basics/operations.js
@@ -0,0 +1,38 @@
+let value=3;
+let negval=-value;
+console.log(negval);//-3 output
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2%2);
+console.log(2/2);
+console.log(2**2);
+
+let str="shubham";
+let str1=" kumar";
+let str2=str+str1;//concatenation
+console.log(str2);
+
+console.log("1"+2);//12
+console.log(1+"2");//12
+console.log("1"+2+2);//122
+console.log(1+2+"2");//32
+
+console.log(true);//true
+console.log(+true);//1
+//console.log(true+);//error
+console.log(+"");//0
+
+let a,b,c,d;
+a=b=c=d=2+2;
+
+a++;//5
+++b;//5
+console.log(a+" "+b);//5 5
+console.log((c++)+" "+(++d));//4 5
+
+/** 
+ * link to study
+ * https://tc39. es/ecma262/multipage/abstract-operations. html#sec-type-conversion
+ */
