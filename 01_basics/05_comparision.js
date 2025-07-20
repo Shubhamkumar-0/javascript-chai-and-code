56b9aa15fbc2ee02d42a1f837a49cc6a5e3b8e05
Shubham Kumar <shubhamkumarerror1@gmail.com>
2025-07-20 17:42:47 +0000

    add comparision3 and 4

--- 01_basics/05_comparision.js
+++ 01_basics/05_comparision.js
@@ -5,3 +5,9 @@ console.log(null>=0); //true
 console.log(null == undefined); // true (both are considered "empty" values)
 console.log(null === undefined); // false (strict equality does not perform type coercion)
 
+console.log(null < 0); // false (null is coerced to 0, and 0 is not less than 0)
+console.log(null <= 0); // true (null is coerced to 0, and 0 is equal to 0)
+
+
+console.log(0 == false); // true (0 is loosely equal to false)
+console.log(0 === false); // false (strict equality does not perform type coercion)
