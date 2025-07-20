218b05a25068b74341f851e7b6cef26ff211512a
Shubham Kumar <shubhamkumarerror1@gmail.com>
2025-07-20 17:40:52 +0000

    add new comparition

--- 01_basics/05_comparision.js
+++ 01_basics/05_comparision.js
@@ -1,3 +1,7 @@
 console.log(null>0); //false
 console.log(null==0); //false
 console.log(null>=0); //true
+
+console.log(null == undefined); // true (both are considered "empty" values)
+console.log(null === undefined); // false (strict equality does not perform type coercion)
+
