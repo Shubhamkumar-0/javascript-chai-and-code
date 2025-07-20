e27f8835f827ca9419ee378376682dedb7eda7a6
Shubham Kumar <shubhamkumarerror1@gmail.com>
2025-07-20 17:43:27 +0000

    explore some more comparision

--- 01_basics/05_comparision.js
+++ 01_basics/05_comparision.js
@@ -9,5 +9,11 @@ console.log(null < 0); // false (null is coerced to 0, and 0 is not less than 0)
 console.log(null <= 0); // true (null is coerced to 0, and 0 is equal to 0)
 
 
-console.log(0 == false); // true (0 is loosely equal to false)
-console.log(0 === false); // false (strict equality does not perform type coercion)
+
+console.log(undefined == null); // true (both are considered "empty" values)
+console.log(undefined === null); // false (strict equality does not perform type coercion)
+
+console.log("5" > 3); // true (string "5" is coerced to number 5)
+console.log("5" < 3); // false (string "5" is coerced to number 5)
+console.log("5" == 5); // true (string "5" is coerced to number 5)
+console.log("5" === 5); // false (strict equality does not perform type coercion)
