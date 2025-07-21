5f5bb6c939eb4b18935cdb1d1a83fac173b043e8
Shubham Kumar <shubhamkumarerror1@gmail.com>
2025-07-21 05:38:40 +0000

    create memory  file and take some example to iunderstand how it works

---
+++ 01_basics/06_memory.js
@@ -0,0 +1,12 @@
+/**  Two type of memory.
+
+stack -> memory use in premetive 
+heap -> memory use in non-premetive.
+
+
+*/
+
+let Name1="shubham"
+let Name2=Name1
+Name2="kumar"
+console.log(Name2)
