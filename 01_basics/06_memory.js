8baf7efc1f7ea30857e78e6a8e8f0608ec377130
Shubham Kumar <shubhamkumarerror1@gmail.com>
2025-07-21 05:50:38 +0000

    explore stack memory allocation

--- 01_basics/06_memory.js
+++ 01_basics/06_memory.js
@@ -7,6 +7,10 @@ heap -> memory use in non-premetive.
 */
 
 let Name1="shubham"
-let Name2=Name1
-Name2="kumar"
-console.log(Name2)
+let Name2=Name1 //shubham
+console.log(Name2)//shubham
+Name2="kumar" //shubham replaced by kumar.
+console.log(Name2)//kumar
+console.log(Name1)//shubham
+Name1=Name2
+console.log(Name1)
