e764d34e8701137630809d2c27b226b3d2619ba9
Shubham Kumar <shubhamkumarerror1@gmail.com>
2025-08-18 10:59:12 +0000

    array done

--- 04_array/01_array.js
+++ 04_array/01_array.js
@@ -19,3 +19,25 @@ console.log(a) //1,2,3,4,5,6
 console.log(typeof a)//string
 
 
+const marvel=["thor","ironman"]
+const dc=["superman"]
+// marvel.push(dc)     //["thor","ironman",["superman"]]
+//generate multiple array so its not a good method.
+// console.log(marvel)
+
+const all=marvel.concat(dc) //[ 'thor', 'ironman', 'superman' ]
+//generate a single new array inside add all the value not the whole arry .its a good method to add the data of one to another array
+console.log(all)
+
+
+const newall=[...marvel,...dc] //[ 'thor', 'ironman', 'superman' ]
+console.log(newall)
+
+
+console.log(Array.isArray("Hitesh"))//false
+console.log(Array.from("Hitesh"))//[ 'H', 'i', 't', 'e', 's', 'h' ]
+
+let score1=100
+let score2=200
+let score3=200
+console.log(Array.of(score1,score2,score3));//[ 100, 200, 200 ]
