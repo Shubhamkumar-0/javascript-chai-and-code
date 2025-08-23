d026189e2c138a781ea6ff3b71a2509fc74f4511
Shubham Kumar <shubhamkumarerror1@gmail.com>
2025-08-23 05:10:05 +0000

    almost complete object

--- 05_objecs/02_objects.js
+++ 05_objecs/02_objects.js
@@ -2,6 +2,15 @@
 // const tinders={}     and ye ek non singleton objects
 // both are same 
 
+const tinders={
+    id:"123abd",
+    email:"tinder@gamil.com",
+    name:"tind",
+    islogged:false,
+} 
+
+
+
 const id={
     email:"shuhu@gmail.com",
     fullname:{
@@ -11,4 +20,44 @@ const id={
         }
     }
 }
-console.log(id.fullname)
+console.log(id.fullname.userfullname.firstname)
+
+const obj1={1:"a",2:"b"}
+const obj2={1:"a",2:"b"}
+// const obj3={obj1,obj2}
+// console.log(obj3) //{ obj1: { '1': 'a', '2': 'b' }, obj2: { '1': 'a', '2': 'b' } }
+
+const obj3=Object.assign(obj1,obj2)
+// const obj3={...obj1,...obj2} both same
+console.log(obj3) //{ '1': 'a', '2': 'b' }
+
+
+// object inside the array
+const users=[
+    {
+        id:1,
+        email:"id1@.com"
+    },
+     {
+        id:2,
+        email:"id2@.com"
+    },
+     {
+        id:3,
+        email:"id3@.com"
+    },
+     {
+        id:4,
+        email:"id4@.com"
+    },
+]
+
+users[1].email
+console.log(tinders);
+console.log(Object.keys(tinders))//return array
+// [ 'id', 'email', 'name', 'islogged' ]
+
+console.log(Object.values(tinders))//return array
+// [ '123abd', 'tinder@gamil.com', 'tind', false ]
+
+console.log(tinders.hasOwnProperty("islooged"));//false
