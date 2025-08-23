516849c2be2c4b85e839eec5ea85bd173c8c2e88
Shubham Kumar <shubhamkumarerror1@gmail.com>
2025-08-23 04:43:57 +0000

    cretaing new file

---
+++ 05_objecs/02_objects.js
@@ -0,0 +1,14 @@
+// const tinder=new Object() ye ek singleton object hai
+// const tinders={}     and ye ek non singleton objects
+// both are same 
+
+const id={
+    email:"shuhu@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname:"shubham",
+            lastname:"kumar"
+        }
+    }
+}
+console.log(id.fullname)
