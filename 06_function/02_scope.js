db6f21642e595833427e00deca229ba5511e4424
Shubham Kumar <shubhamkumarerror1@gmail.com>
2025-08-29 05:24:53 +0000

    creatinf a file for scope and write code for local and globle scope and varibles

---
+++ 06_function/02_scope.js
@@ -0,0 +1,19 @@
+// {} ---> this is scope.
+if(true){
+    let a=10;
+    const b=20;
+    var c=30;
+}
+// console.log(a)// error
+// console.log(b)//error
+console.log(c)//30
+
+// GLOBLE SCOPE:->Variables declared in the global scope are accessible from anywhere in your code, including inside functions.
+// LOCAL SCOPE:->ariables declared within a function (or block) are local to that function and cannot be accessed from outside it. This is known as function scope or block scope (for let and const).
+
+let a=300;
+if(true){
+    let a=20;
+    console.log("inner:",a);//20-->local scopr variable access
+}
+console.log("outer:",a);//300 --> global scope variable access
